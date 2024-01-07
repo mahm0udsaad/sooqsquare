21c01a69132ff33e1e8b834a5f7ae4be40c028a3
unknown <saad123mn123@gmail.com>
2024-01-07 18:32:07 +0200

    delete clerk

--- app/[lng]/myAds/page.jsx
+++ app/[lng]/myAds/page.jsx
@@ -1,14 +1,14 @@
 
-import DynamicCard from '../../../components/adCard'
-import prisma from "../../../prisma/client";
+// import DynamicCard from '../../../components/adCard'
+// import prisma from "../../../prisma/client";
 
 export default async function MyAds() {
-    const ads = await prisma.Ad.findMany({});
+    // const ads = await prisma.Ad.findMany({});
   return (
     <main className=" mx-8 grid grid-cols-2 pt-20">
-       {ads.map((ad) => (
+       {/* {ads.map((ad) => (
         <DynamicCard key={ad.id} ad={ad} />
-      ))}
+      ))} */}
     </main>
   )
 }
