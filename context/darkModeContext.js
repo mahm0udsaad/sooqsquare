f7d31d35e95c3ffcd5b87907ab11e0269679303c
unknown <saad123mn123@gmail.com>
2024-01-27 19:37:18 +0200

    u

--- context/darkModeContext.js
+++ context/darkModeContext.js
@@ -114,7 +114,7 @@ export const DarkModeProvider = ({ children }) => {
 
   return (
     <DarkModeContext.Provider value={{isConfettiActive, setConfettiActive , darkMode, setDarkMode ,countryName,phoneNum , setPhoneNum , extraFeature ,setExtraFeature,errorMessage , setErrorMessage , successMessage, setSuccessMessage, adImages ,setAdImages }}>
-      <div className='fixed top-0 left-0 w-full h-full pointer-events-none z-50' ref={containerRef} id="confetti-container"></div>
+      <div className='fixed top-0 left-0 w-full h-full pointer-events-none' ref={containerRef} id="confetti-container"></div>
       {children}
     </DarkModeContext.Provider>
   );
