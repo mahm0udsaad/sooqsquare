506de14dd7fae06843948e6371f1cde826595280
unknown <saad123mn123@gmail.com>
2024-02-19 21:39:16 +0200

    fix

--- app/[lng]/(traderDashboard)/dashboard/page.jsx
+++ app/[lng]/(traderDashboard)/dashboard/page.jsx
@@ -1,14 +1,15 @@
 import Dashboard from "../../../../components/component/shops-dashboard";
-import { getServerSession } from 'next-auth'
-import { getUserByEmail } from '@/prisma/actions'
+import { getServerSession } from "next-auth";
+import { getUserByEmail } from "@/prisma/actions";
+import { redirect } from "next/navigation";
 
 const MainDashboard = async () => {
-    const logedUser = await getServerSession()
-    const user = await getUserByEmail(logedUser?.user.email)
-    
-   return (
-       <Dashboard shops={user.shop}/>
-    );
-}
+  const logedUser = await getServerSession();
+  const user = await getUserByEmail(logedUser?.user.email);
+  if (!user) {
+    redirect("/sign-in");
+  }
+  return <Dashboard shops={user?.shop} />;
+};
 
 export default MainDashboard;
